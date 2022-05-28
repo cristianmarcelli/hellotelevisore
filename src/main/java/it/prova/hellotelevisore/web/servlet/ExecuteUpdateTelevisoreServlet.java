2803f5482b30c4ce6c9e3ec1e6d8f03b173058cc
Cristian <cristianma98@gmail.com>
2022-05-28 17:57:08 +0200

    completata e testata ExecuteUpdateTelevisoreServlet

--- src/main/java/it/prova/hellotelevisore/web/servlet/ExecuteUpdateTelevisoreServlet.java
+++ src/main/java/it/prova/hellotelevisore/web/servlet/ExecuteUpdateTelevisoreServlet.java
@@ -1,24 +1,51 @@
 package it.prova.hellotelevisore.web.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import it.prova.hellotelevisore.model.Televisore;
+import it.prova.hellotelevisore.service.MyServiceFactory;
+
 @WebServlet("/ExecuteUpdateTelevisoreServlet")
 public class ExecuteUpdateTelevisoreServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String marca = request.getParameter("marcaInput");
+		String modello = request.getParameter("modelloInput");
+		Integer prezzo = Integer.parseInt(request.getParameter("prezzoInput"));
+		Integer numeroPollici = Integer.parseInt(request.getParameter("numeroPolliciInput"));
+		String codice = request.getParameter("codiceInput");
+
+		try {
+			Televisore televisoreDaRecuperare = MyServiceFactory.getTelevisoreServiceInstance()
+					.caricaSingoloTelevisore(Long.parseLong(request.getParameter("idTelevisore")));
+
+			televisoreDaRecuperare.setMarca(marca);
+			televisoreDaRecuperare.setModello(modello);
+			televisoreDaRecuperare.setPrezzo(prezzo);
+			televisoreDaRecuperare.setNumeroPollici(numeroPollici);
+			televisoreDaRecuperare.setCodice(codice);
+
+			request.setAttribute("listaTelevisoriAttributeName",
+					MyServiceFactory.getTelevisoreServiceInstance().elencaTutti());
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		RequestDispatcher rd = request.getRequestDispatcher("results.jsp");
+		rd.forward(request, response);
+
 	}
 
 }
