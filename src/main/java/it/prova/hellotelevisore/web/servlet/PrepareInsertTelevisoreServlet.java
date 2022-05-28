e855550b434cd38bff51086f7685aafeb540d6b3
Cristian <cristianma98@gmail.com>
2022-05-28 15:30:33 +0200

    inserite PrepareInsertTelevisoreServlet e insert.jsp vuote e testate se collegate al progetto

---
+++ src/main/java/it/prova/hellotelevisore/web/servlet/PrepareInsertTelevisoreServlet.java
@@ -0,0 +1,26 @@
+package it.prova.hellotelevisore.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/PrepareInsertTelevisoreServlet")
+public class PrepareInsertTelevisoreServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("prova prepareInsert").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
